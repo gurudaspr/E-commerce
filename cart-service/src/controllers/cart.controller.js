c8bcb6c946e350e9c5c01ad54555faab910c049b
gurudaspr <gurudaspranavam007@gmail.com>
2024-08-27 12:32:30 +0530

    Enhance Cart Service Functionality
    - Updated `cart.controller.js` to improve item handling and validation.
    - Modified `cart.routes.js` to include new route functionalities for cart management.

--- cart-service/src/controllers/cart.controller.js
+++ cart-service/src/controllers/cart.controller.js
@@ -11,7 +11,14 @@ export const addCart = async (req, res) => {
 
         const existingCart = await Cart.findOne({ user: userId });
         if (existingCart) {
-
+            // Check if the product is already in the cart
+            const existingProuct = existingCart.cartItems.find(item => item.product.toString() === productId);
+            if (existingProuct) {
+                existingProuct.quantity += 1;
+                await existingCart.save();
+                return res.status(201).json(existingCart);
+            }
+            // Add the product to the  empty cart
             existingCart.cartItems.push({
                 product: productId,
                 createdAt: Date.now()
@@ -19,6 +26,7 @@ export const addCart = async (req, res) => {
             await existingCart.save();
             return res.status(201).json(existingCart);
         }
+        // Create a new cart and add the product to it
         const cart = new Cart({
             user: userId,
             cartItems: [{
@@ -39,12 +47,12 @@ export const addCart = async (req, res) => {
 
 export const getCartByUser = async (req, res) => {
     // Use a fixed userId for testing
-    const userId = "66c573ffbdbd6696e1585149";
+    const { userId } = req.user;
     try {
         const cart = await Cart.findOne({ user: userId });
         if (!cart) {
             return res.status(404).json({ message: 'Cart not found' });
-        }   
+        }
         const allProductsId = cart.cartItems.map(item => item.product);
         console.log("Product IDs:", allProductsId);
 
@@ -60,3 +68,59 @@ export const getCartByUser = async (req, res) => {
         return res.status(500).json({ message: 'Error getting cart', error: err.message });
     }
 };
+
+
+export const updateCart = async (req, res) => {
+    const { userId } = req.user;
+    console.log(userId, 'userId');
+    const { productId, status } = req.query;
+    console.log(req.params, 'params');
+    try {
+        const cart = await Cart.findOne({ user: userId });
+        if (!cart) {
+            return res.status(404).json({ message: 'Cart not found' });
+        }
+        const cartItemIndex = cart.cartItems.findIndex(item => item.product.toString() === productId);
+        if (cartItemIndex === -1) {
+            return res.status(404).json({ message: 'Cart item not found' });
+        }
+
+        if (status === 'inc') {
+            cart.cartItems[cartItemIndex].quantity += 1;
+        } else if (status === 'dec') {
+            if (cart.cartItems[cartItemIndex].quantity > 1) {
+                cart.cartItems[cartItemIndex].quantity -= 1;
+            } else {
+                cart.cartItems.splice(cartItemIndex, 1);
+            }
+        }
+        await cart.save();
+        return res.status(200).json(cart);
+    } catch (err) {
+        console.error("Error in updating cart:", err.message);
+        return res.status(500).json({ message: 'Error updating cart', error: err.message });
+    }
+};
+
+//remove item from cart
+
+export const removeItemFromCart = async (req, res) => {
+    const { userId } = req.user;
+    const { productId } = req.params;
+    try {
+        const cart = await Cart.findOne({ user: userId });
+        if (!cart) {
+            return res.status(404).json({ message: 'Cart not found' });
+        }
+        const cartItemIndex = cart.cartItems.findIndex(item => item.product.toString() === productId);
+        if (cartItemIndex === -1) {
+            return res.status(404).json({ message: 'Cart item not found' });
+        }
+        cart.cartItems.splice(cartItemIndex, 1);
+        await cart.save();
+        return res.status(200).json(cart);
+    } catch (err) {
+        console.error("Error in removing item from cart:", err.message);
+        return res.status(500).json({ message: 'Error removing item from cart', error: err.message });
+    }
+}
--- cart-service/src/routes/cart.routes.js
+++ cart-service/src/routes/cart.routes.js
@@ -1,14 +1,13 @@
 import express from "express";
-import { addCart, getCartByUser } from "../controllers/cart.controller.js";
+import { addCart, getCartByUser, removeItemFromCart, updateCart } from "../controllers/cart.controller.js";
 import { verifyJwtToken } from "../middlewares/verifyJwtToken.js";
 
 
 const router = express.Router();
 
 
-router.get('/',verifyJwtToken, getCartByUser)   // Get all carts
-router.post('/:productId', verifyJwtToken,  addCart)  // Add a new cart
-// router.put('/:productId', ) // Update a cart
-// router.delete('/:productId', ) // Delete a cart
-// router.get('/:id', ) // Get a cart by id
+router.get('/', verifyJwtToken, getCartByUser)   // Get all carts
+router.post('/:productId', verifyJwtToken, addCart)  // Add a new cart
+router.patch('/', verifyJwtToken, updateCart) // Update a product in a cart
+router.delete('/:productId', verifyJwtToken, removeItemFromCart) // Delete an item from a cart
 export default router;
